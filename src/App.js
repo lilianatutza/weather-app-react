b58828328b51572511928f05b084450a072483c6
Liliana Tuta <lilianatutza@gmail.com>
2024-11-26 18:51:18 +0000

    changed name of City to City1

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import Form from "./components/Form";
 import Humidity from "./components/Humidity";
 import Temperature from "./components/Temperature";
 import Updates from "./components/Updates";
-import City from "./components/City";
+import City1 from "./components/City1";
 import Weather from "./components/Weather";
 import SearchEngine from "./components/SearchEngine";
 
 export default function App() {
-  const humidity = 87; // percent 
+  const humidity = 87; // percent
   const wind = 7.2; // km/h
 
   return (
     <div className="App">
       <Form />
-      <City />
+      <City1 />
       <Updates />
       <Temperature />
       <Humidity humidity={humidity} wind={wind} />
